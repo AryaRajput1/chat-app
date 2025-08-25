af50a09ab890e63e711abe79e6270cc938540427
Arya Rajput <rajputarya10@gmail.com>
2025-08-25 09:00:36 +0530

    feat: add cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,10 +3,16 @@ import authRoute from "./routes/auth.route";
 import messageRoute from "./routes/message.route";
 import 'dotenv/config';
 import { connectToDB } from "./config/db";
+import cors from 'cors'
 
 const PORT = process.env.PORT || 8081;
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.UI_URL,
+  credentials: true
+}))
 
 app.use(express.json());
 
