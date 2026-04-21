2765a013473958480cfec2f952120193d1585bce
Arya Rajput <rajputarya10@gmail.com>
2026-04-22 00:32:42 +0530

    feat: add routing in client side

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div>Client UI</div>
-  )
-}
-
-export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignIn from "./pages/auth/SignIn.tsx";
+import SignUp from "./pages/auth/SignUp.tsx";
+import Home from "./pages/home/Home.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+
+        {/* Protected Route */}
+        <Route path="/" element={<Home/>} />
+        {/* /fallback page */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
